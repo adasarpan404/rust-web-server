56820df37987ee528946b3d3499f00f0bcd2d6f9
Arpan Das <dasarpan471@gmail.com>
2024-08-07 19:52:26 +0530

    main

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,12 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{web, App, HttpServer, Responder};
 
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+async fn index() -> impl Responder {
+    "Hello World"
 }
-
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
-
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
-}
-
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
-        App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
+        App::new().service(web::scope("/app").route("/index.html", web::get().to(index)))
     })
     .bind(("127.0.0.1", 3000))?
     .run()
