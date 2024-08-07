7d3ec0fda54be63f93996680723a267caad06248
Arpan Das <dasarpan471@gmail.com>
2024-08-07 20:56:21 +0530

    Shared Mutable State

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,24 @@
 use actix_web::{get, web, App, HttpServer};
+use std::sync::Mutex;
 
-struct AppState {
-    app_name: String,
+struct AppStateWithCounter {
+    counter: Mutex<i32>,
 }
+
 #[get("/")]
-async fn index(data: web::Data<AppState>) -> String {
-    let app_name = &data.app_name;
-    format!("Hello {app_name}")
+async fn index(data: web::Data<AppStateWithCounter>) -> String {
+    let mut counter = data.counter.lock().unwrap();
+    *counter += 1;
+    format!("Request number: {counter}")
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .app_data(web::Data::new(AppState {
-                app_name: String::from("Arpan Das"),
-            }))
-            .service(index)
-    })
-    .bind(("127.0.0.1", 3000))?
-    .run()
-    .await
+    let counter = web::Data::new(AppStateWithCounter {
+        counter: Mutex::new(0),
+    });
+    HttpServer::new(move || App::new().app_data(counter.clone()).service(index))
+        .bind(("127.0.0.1", 3000))?
+        .run()
+        .await
 }
